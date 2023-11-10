5c3959db5b28a1fb5e8d678052001cd94ec2813e
shivraj <shivraj@micronxt.com>
2023-11-10 08:52:01 +0530

    last commit

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,35 @@
 // to verify the JWT token and also user is Admin or Not
+// first we need to check user or admin is sending the token or Not
+// if sending decode the token and verify
 
 const User = require("../model/userModel");
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
+
+const authMiddleware = asyncHandler(async (req, res, next) => {
+  let token;
+  if (req?.headers?.authorization?.startsWith("Bearer")) {
+    token = req.headers.authorization.split(" ")[1];
+    try {
+      if (token) {
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decode?.id);
+        req.user = user;
+        next();
+      }
+    } catch (error) {
+      throw new Error("Not Authorized, Token Expired, Login Again");
+    }
+  } else {
+    throw new Error("There is no token attached to header");
+  }
+});
+
+const isAdmin = asyncHandler(async (req, res, next) => {
+    console.log(req.user)
+});
+
+module.exports = {
+  authMiddleware,
+  isAdmin,
+};
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const {createUser, loginUserCtrl, getallUser, getSingleUser, deleteaUser, updateaUser} = require('../controller/userCtrl')
+const {authMiddleware, isAdmin} = require("../middleware/authMiddleware");
 
 router.post("/register", createUser);
 router.post("/login", loginUserCtrl);
 router.get("/all-users", getallUser);
-router.get("/:id", getSingleUser);
+router.get("/:id", authMiddleware, isAdmin, getSingleUser);
 router.delete("/:id", deleteaUser);
 router.put("/:id", updateaUser);
 
